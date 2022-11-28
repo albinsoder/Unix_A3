6e81e16af0eb8013bc9e5a98fc5ad35174ca7932
Albin Södervall <91470009+albinsoder@users.noreply.github.com>
2022-11-28 13:51:22 +0100

    update

---
+++ assembly_files/looptest.s
@@ -0,0 +1,33 @@
+	.data
+a:      .quad   0
+b:      .quad   0
+i:      .quad   0
+s:      .quad   0
+n:      .quad   0
+result: .ascii "Result: %d\n"  
+	.text
+	.global	main
+main:
+	movq	$100, %rax
+	movq	%rax, i
+L000:
+	movq	i, %rax
+	movq	$0, %rdx
+	cmpq	%rax, %rdx
+	je	L001
+	movq	i, %rdx
+	movq	%rax, %rdx
+	pushq	%rbp
+	leaq	result(%rip), %rdi
+	movq	%rdx, %rsi
+	xorq	%rax,  %rax
+	call	printf
+	popq	%rbp
+	movq	i, %rax
+	movq	$1, %rax
+	subq	%rdx, %rax
+	movq	%rax, i
+	jmp	L000
+L001:
+    leave
+	ret
